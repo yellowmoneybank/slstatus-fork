f36da137a2e9b9ac1f144f61c57c6e97f51d154b
Moritz Müller <moritzm.mueller@posteo.de>
2020-09-23 11:42:06 +0200

    config

--- config.def.h
+++ config.def.h
@@ -61,7 +61,11 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
+
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
-};
+    /* function format          argument */
+    {battery_perc, "b: %s%% | ", "BAT0"},
+    {cpu_perc, "c: %s%% | ", NULL},
+    {ram_perc, "r: %s%% | ", NULL},
+    {datetime, "%s", "%F %T"},
+}
